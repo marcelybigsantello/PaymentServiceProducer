24c714ec9cda1b9ad62f2dd6aa357b89b5427b04
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2025-04-22 01:07:36 -0300

    Correcting final price calculation based on product's quantity

--- src/main/java/com/masantello/paymentserviceproducer/services/impl/CalculateServiceImpl.java
+++ src/main/java/com/masantello/paymentserviceproducer/services/impl/CalculateServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.masantello.paymentserviceproducer.models.Payment;
-import com.masantello.paymentserviceproducer.models.Product;
 import com.masantello.paymentserviceproducer.services.CalculateService;
 
 @Service
@@ -16,7 +15,7 @@ public class CalculateServiceImpl implements CalculateService {
 		return Optional.ofNullable(payment)
 				.map(Payment::getProducts)
 				.map(products -> products.stream()
-						.mapToDouble(Product::getPrice)
+						.mapToDouble(product -> product.calculateTotalPrice())
 						.sum())
 				.orElse(0.0)
 				.floatValue();
